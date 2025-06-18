234173baf25ccaef0166a5dfa21859f55c681fc5
brahmasai <brahmasaicloud@gmail.com>
2025-06-18 12:19:37 +0530

    v5

--- javascript_tutorial/src/Arrays/Array_itteration.ts
+++ javascript_tutorial/src/Arrays/Array_itteration.ts
@@ -1,7 +1,7 @@
 
 
-const num = [1,2,3,4,5,6,7,8]
-const str = ["ram", "prasad", "brahma","sai", "phani","mohana"]
+// const num = [1,2,3,4,5,6,7,8]
+// const str = ["ram", "prasad", "brahma","sai", "phani","mohana"]
 // // foreach
 // // Purpose: Executes a function for each element in an array.
 // // Returns: Nothing (undefined); it's used for side effects (e.g., logging, updating UI).
@@ -59,13 +59,14 @@ const str = ["ram", "prasad", "brahma","sai", "phani","mohana"]
 //   return newAccumulator;
 // }, initialValue);
 
-// accumulator: Carries the result through each iteration.
-// currentValue: The current item being processed.
+// accumulator: stores the ongoing total.
+// currentValue: is  the current number in the loop.
 // initialValue: The starting value for the accumulator (e.g. 0, {}, []).
 
 // const numbers = [1, 2, 3, 4];
-// const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-// console.log(sum); 
+// const sum = numbers.reduce((a,c)=> a+c ,0)
+// console.log(sum);
+
 
 // // in comparsion operator  >  < 
 // // print output in boolean
@@ -96,3 +97,64 @@ const str = ["ram", "prasad", "brahma","sai", "phani","mohana"]
 // console.log(): just print result each element line by line 
 
 // result : just store the true result of each element make in an respective inputs (arrays , string , numbers , objects)
+
+// =============================================================
+// Array.prototype  : it create a custom method to Array.prototype
+//================================================================
+// Extend Array prototype
+// Array.prototype.first = function () {
+//   return this[];
+// };
+
+// // Tell TypeScript about your custom method
+// declare global {
+//   interface Array<T> {
+//     first(): T | undefined;
+//   }
+// }
+
+// export {};
+
+// const arr = [10, 20, 30];
+// console.log(arr.first()); // Output: 10
+
+
+// ======================================================
+// array iterations practice 
+
+
+const num = [1,2,3,4,5,6,7]
+const str = ["brahmasai", "rp","phani"]
+
+// foreach
+// num.forEach((x)=>console.log(x))
+
+//map
+// num.map((x)=> console.log(x))
+
+// map returns new array  
+// const numMap = num.map((x)=> x)
+// console.log(numMap)
+
+// is map returns an  array based on condition 
+
+// num.map((x)=> console.log(x>2))
+
+// const nummapcondition = num.map ((x)=> x>3)
+// console.log(nummapcondition)       // map return a new array what value u get, while useing comparsion operator result must be in boolean only 
+
+//filter returns new array based on condition 
+
+// num.filter((x)=> console.log(x))
+// const numfilter = num.filter(x=> x)
+// console.log(numfilter)
+
+// num.filter(x => console.log(x>3))
+
+// const numCondition =  num.filter(x => x>3)
+// console.log(numCondition)  // based on condition that true assigned values only shows 
+
+// reduce
+// const sum = num.reduce((a,c)=>a+c,0)
+// console.log(sum)
+
